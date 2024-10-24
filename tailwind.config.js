87f393104ed05b89b4aee113deddb46aed8f6399
Bzzzang <sindmsql@gmail.com>
2024-10-24 16:14:21 +0900

    테마 환경구성 - 웹폰트 사용하기

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "1600px",
+      },
+      width: {
+        100: "25rem",
+        128: "32rem",
+      },
+      fontFamily: {
+        suit: ['"SUIT Variable", "sans-serif"'],
+      },
+      fontSize: {
+        "10xl": [
+          "10rem",
+          {
+            lineHeight: 1,
+            letterSpacing: "-0.02em",
+          },
+        ],
+      },
+    },
   },
   plugins: [],
 };
